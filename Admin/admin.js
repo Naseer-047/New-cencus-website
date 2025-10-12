5d1af3e4bea30450d906fe5e73cca61d5b7cd320
Naseer-047 <n763122562@gmail.com>
2025-10-12 10:42:45 +0530

    -----------------

--- Admin/admin.js
+++ Admin/admin.js
@@ -1,4 +1,4 @@
- wrongPasswordCount= localStorage.getItem('wrongpss') || 0;
+ wrongPasswordCount= localStorage.getItem('wrongpss') || 1;
 
 function crossclick(){
   document.querySelector('.alertmsg').style.display='none';
@@ -42,9 +42,9 @@ if(wrongPasswordCount>5){
    document.getElementById('toenbtn')
    toenbtn.style.display='inline';
       function tokbtn(){
-        if (token='abc') {
+        if (token==6362012799) {
           alert('token validation successfull')
-         wrongPasswordCount=0;
+         wrongPasswordCount=1;
          localStorage.setItem('wrongpss',wrongPasswordCount)
          location.reload();
         }
